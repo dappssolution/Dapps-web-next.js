98004c3896dc95f9ba004a8d45b3318926ed7cc9
dappssolution <dappssolution04@gmail.com>
2026-02-18 12:45:42 +0530

    the changes are layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -198,15 +198,15 @@ export default function RootLayout({
 <Script id="meta-pixel" strategy="afterInteractive">
   {`
     !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1529388994822608');
-    fbq('track', 'PageView');
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1529388994822608');
+fbq('track', 'PageView');
   `}
 </Script>
 
@@ -224,22 +224,17 @@ export default function RootLayout({
          {/* Google Tag Manager (noscript)  */}
           
           <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NVL7HVHT"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* Meta Pixel (noscript) */}
-<noscript>
-  <img
+  <img 
     height="1"
     width="1"
-    style={{ display: "none" }}
+    style={{ display: "none" }} 
     src="https://www.facebook.com/tr?id=1529388994822608&ev=PageView&noscript=1"
   />
+
 </noscript>
+         
+     
+
 
 
         {/* End Google Tag Manager (noscript) */}
