c219a1eaca8c3648ce785abdee3c59597a9caf12
Ameen VP <mhdameenvp@gmail.com>
2026-03-06 14:15:30 +0530

    faq update

--- src/app/service/erp-crm/ErpCrmClient.tsx
+++ src/app/service/erp-crm/ErpCrmClient.tsx
@@ -4,6 +4,8 @@ import ServiceDetailBanner from "@/components/services-innerpages/ServiceDetailB
 import ServiceDetailMain from "@/components/services-innerpages/ServiceDetailMain";
 import { serviceDetails } from "../service-detail-contents";
 import { useLanguage } from "@/contexts/LanguageContext";
+import FaqAccordion from "@/components/faqAccordion";
+import { erpCrmFaqData } from "@/components/erpCrmFaqData";
 
 export default function ErpCrmClient() {
   const { language } = useLanguage();
@@ -13,6 +15,10 @@ export default function ErpCrmClient() {
       <ServiceDetailBanner {...content.banner} />
       <ServiceDetailMain {...content.main} />
       <ServiceDetailAccordion {...content.accordion} />
+      {/* FAQ Section for ERP & CRM */}
+      <section className="max-w-5xl mx-auto my-16 px-4">
+        <FaqAccordion faqItems={erpCrmFaqData} />
+      </section>
     </>
   );
 }
